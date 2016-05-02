d241dda41fe8850e1d47e34b4838ef39ea2873e6
insookyo <dlstnry123@gmail.com>
2016-05-02 23:58:33 +0900

    make image black and resizing and elastic distortion and pickle

--- src/project2_svm.py
+++ src/project2_svm.py
@@ -1,30 +1,39 @@
 from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score, accuracy_score
 from sklearn import svm
-
+import os
 from utility import load_images
+from PIL import Image
+import numpy as np
+
+train_dir = os.path.join(os.path.dirname(__file__), '../data', 'train')
+test_dir = os.path.join(os.path.dirname(__file__), '../data', 'test')
 
 
-train_dir = '../data/train'
-test_dir = '../data/test'
+def load_data():
+    train_X, train_Y = load_images(train_dir, mode="train")
+    test_X, test_Y = load_images(test_dir, mode="test")
+    # train_X, train_Y = test_X, test_Y
 
-train_X, train_Y = load_images(train_dir)
-test_X, test_Y = load_images(test_dir)
+    return train_X, train_Y, test_X, test_Y
 
-# training phase
-## TODO
-# change training method
-print 'training phase'
-clf = svm.SVC()
-clf.fit(train_X, train_Y)
+def feature_engineering(train_X):
+    return train_X
 
-# predict phase
-print 'test phase'
-predicts = clf.predict(test_X)
 
-# measure function
-print 'measure phase'
-print confusion_matrix(test_Y, predicts)
-print f1_score(test_Y, predicts, average=None)
-print precision_score(test_Y, predicts, average=None)
-print recall_score(test_Y, predicts, average=None)
-print accuracy_score(test_Y, predicts)
+if __name__ == '__main__':
+    train_X, train_Y, test_X, test_Y = load_data()
+    print "data load complete"
+    train_X = feature_engineering(train_X)
+
+    print 'training phase'
+    clf = svm.LinearSVC().fit(train_X, train_Y)
+    print 'test phase'
+    predicts = clf.predict(test_X)
+
+    # measure function
+    print 'measure phase'
+    print confusion_matrix(test_Y, predicts)
+    print f1_score(test_Y, predicts, average=None)
+    print precision_score(test_Y, predicts, average=None)
+    print recall_score(test_Y, predicts, average=None)
+    print accuracy_score(test_Y, predicts)
--- src/utility.py
+++ src/utility.py
@@ -3,32 +3,132 @@ from os.path import join
 
 import numpy as np
 from PIL import Image
+import math
+from scipy.linalg import norm
+from scipy.ndimage.filters import gaussian_filter
 
 
-def load_images(base_dir):
-    image_names = listdir(base_dir)
+def bilinear_interpolate(values, dx, dy):
+    """Interpolating with given dx and dy"""
+    assert values.shape == dx.shape == dy.shape
 
-    X = []
-    Y = np.empty(0)
-    for i, image_name in enumerate(image_names):
-        image_path = join(base_dir, image_name)
-        y = image_name.split('-')[0][3:]
-        y = int(y)
+    A = np.zeros(values.shape)
+    for i in range(values.shape[0]):
+        for j in range(values.shape[1]):
+            x = i + dx[i, j]
+            y = j + dy[i, j]
+            if x < 0:
+                x = x + int(1 + 0 - x)
+            if x >= values.shape[0] - 1:
+                x = x - int(1 + x - (values.shape[0] - 1))
+            if y < 0:
+                y = y + int(1 + 0 - y)
+            if y >= values.shape[1] - 1:
+                y = y - int(1 + y - (values.shape[1] - 1))
 
-        image = Image.open(image_path)
-        im = np.array(image, dtype=np.int16, copy=True).transpose((1,0,2))
-        image.close()
+            x1 = int(x)
+            y1 = int(y)
+            x2 = x1 + 1
+            y2 = y1 + 1
+            f11 = values[x1, y1]
+            f12 = values[x1, y2]
+            f21 = values[x2, y1]
+            f22 = values[x2, y2]
 
-        # TODO
-        # change below - preprocessing for image file
-        # memory will be explode if you are not doing dimension reduction and copy ( 2*1200*900 = 2.06MB, numpy object = 26MB per image)
-        shape = im.shape
-        im = np.reshape(im, shape[0]*shape[1]*shape[2])
-        im = np.copy(im[0:10]) # MUST
-        Y = np.append(Y, y)
-        X.append(im)
+            A[i, j] = (
+                f11*(x2-x)*(y2-y) + f12*(x2-x)*(y-y1)
+                + f21*(x-x1)*(y2-y) + f22*(x-x1)*(y-y1)
+            )
+    return A
+
+def elastic_distortion(image, sigma=5, alpha=36):
+    def delta():
+        d = gaussian_filter(np.random.uniform(-1, 1, size=image.shape), sigma)
+        return (d / norm(d)) * alpha
+
+    assert image.ndim == 2
+    dx = delta()
+    dy = delta()
+    return bilinear_interpolate(image, dx, dy)
+
+
+def load_images(base_dir, resize_shape=64, mode="train"):
+    if mode == "train":
+        with open(os.path.join(os.path.dirname(__file__), '../data', 'train',"X.pkl"), 'rb') as f:
+            X = cPickle.load(f)
+        with open(os.path.join(os.path.dirname(__file__), '../data', 'train',"Y.pkl"), 'rb') as f:
+            Y = cPickle.load(f)
+
+    else:
+        image_names = listdir(base_dir)
+
+        X = []
+        Y = np.empty(0)
+        for i, image_name in enumerate(image_names):
+            image_path = join(base_dir, image_name)
+            y = image_name.split('-')[0][3:]
+            y = int(y)
+            image = Image.open(image_path)
+
+            # change to black image
+            black_image = image.convert('L')
+            # resize the image
+            black_resized_image = black_image.resize((resize_shape, resize_shape))
+            # black_resized_image.show()
+            # image class to array
+            im = np.array(black_resized_image, dtype=np.int16, copy=True)
+
+            if mode == "save":
+                 # affine transformation
+                im_elastic0 = elastic_distortion(im, sigma=0.5)
+                im_elastic1 = elastic_distortion(im, sigma=1)
+                im_elastic2 = elastic_distortion(im, sigma=2)
+                im_elastic3 = elastic_distortion(im, sigma=3)
+                # Image.fromarray(im_elastic3).show()
+            image.close()
+
+            # TODO
+            # change below - preprocessing for image file
+            # memory will be explode if you are not doing dimension reduction and copy ( 2*1200*900 = 2.06MB, numpy object = 26MB per image)
+            shape = im.shape
+            im = np.reshape(im, shape[0]*shape[1])
+            # im = np.copy(im) # MUST
+            # print im.shape
+            Y = np.append(Y, y)
+            X.append(im)
+
+            if mode == "save":
+                im_elastic0 = np.reshape(im_elastic0, shape[0]*shape[1])
+                X.append(im_elastic0)
+                Y = np.append(Y, y)
+
+                im_elastic1 = np.reshape(im_elastic1, shape[0]*shape[1])
+                X.append(im_elastic1)
+                Y = np.append(Y, y)
+
+                im_elastic2 = np.reshape(im_elastic2, shape[0]*shape[1])
+                X.append(im_elastic2)
+                Y = np.append(Y, y)
+
+                im_elastic3 = np.reshape(im_elastic3, shape[0]*shape[1])
+                X.append(im_elastic3)
+                Y = np.append(Y, y)
+
+        if mode == "save":
+            with open(os.path.join(os.path.dirname(__file__), '../data', 'train',"X.pkl"), 'wb+') as f:
+                cPickle.dump(X, f)
+            with open(os.path.join(os.path.dirname(__file__), '../data', 'train',"Y.pkl"), 'wb+') as f:
+                cPickle.dump(Y, f)
 
     return np.array(X), Y
 
 
+if __name__ == "__main__":
+    import os
+    import cPickle
 
+    train_dir = os.path.join(os.path.dirname(__file__), '../data', 'train')
+    test_dir = os.path.join(os.path.dirname(__file__), '../data', 'test')
+    X, Y = load_images(train_dir, mode="train")
+    print X.shape
+    print Y.shape
